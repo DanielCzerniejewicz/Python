e2b03da04473224cd784fdac78f93428944c9096
Daniel Czerniejewicz <61429464+DanielCzerniejewicz@users.noreply.github.com>
2023-05-22 10:18:24 +0200

    Update Szwajcarskie.py
    Dodano tworzenie pliku szwajcaria.txt

--- Szwajcarskie.py
+++ Szwajcarskie.py
@@ -1,16 +1,22 @@
-import math
 def sumacyfr(n):
-    suma = 0
-    while n != 0:
-        suma += n % 10
-        n = n // 10
-    return suma
+  suma = 0
+  while n > 0:
+    suma += n % 10
+    n = n // 10
+  return suma
 
 
+import math
+import os
+
+os.remove("szwajcaria.txt")
+file = open("szwajcaria.txt", "x")
 for i in range(10, 100):
-    gora = sumacyfr(i)
-    dol = i
-    gcd = math.gcd(gora,dol)
-    gora2 = gora//gcd
-    if gora2 == 1:
-        print(gora2 , dol//gcd)
+  gora = sumacyfr(i)
+  dol = i
+  gcd = math.gcd(gora, dol)
+  gora2 = gora // gcd
+  dol2 = dol // gcd
+  if gora2 == 1:
+    file.write(f"{gora}/{dol} = {gora2}/{dol2}" + "\n")
+file.close()
